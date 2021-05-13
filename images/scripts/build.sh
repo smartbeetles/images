94df1dec1479b3edd0961e8de9bcfefc587222da
A.Treitjak <a.treitjak@gmail.com>
2021-05-13 15:48:09 +0300

    Build updated

--- images/scripts/build.sh
+++ images/scripts/build.sh
@@ -29,11 +29,13 @@ if [[ -z ${SEARCH_PATH} ]]; then
 fi
 
 ## login to docker hub as needed
-if [[ $PUSH_FLAG ]]; then
-  if [ -t 1 ]; then
-    docker login
-  else
-    echo "${DOCKER_PASSWORD:-}" | docker login -u "${DOCKER_USERNAME:-}" --password-stdin
+if [[ ${PUSH_FLAG} ]]; then
+  if [[ ${DOCKER_USERNAME:-} ]]; then
+    echo "Attempting non-interactive docker login (via provided credentials)"
+    echo "${DOCKER_PASSWORD:-}" | docker login -u "${DOCKER_USERNAME:-}" --password-stdin ${DOCKER_REGISTRY:-docker.io}
+  elif [[ -t 1 ]]; then
+    echo "Attempting interactive docker login (tty)"
+    docker login ${DOCKER_REGISTRY:-docker.io}
   fi
 fi
 
